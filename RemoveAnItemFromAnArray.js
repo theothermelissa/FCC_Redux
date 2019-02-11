b5aae6549fe30a41d0c32252e9fe6a3f76ae7317
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-02-11 16:47:17 -0600

    Use slice to remove an array item by index

---
+++ RemoveAnItemFromAnArray.js
@@ -0,0 +1,21 @@
+const immutableReducer = (state = [0,1,2,3,4,5], action) => {
+  switch(action.type) {
+    case 'REMOVE_ITEM':
+      // don't mutate state here or the tests will fail
+      let length = state.length;
+      let i = action.index;
+      return state.slice(0, i).concat(state.slice(i+1, length));
+      break; 
+    default:
+      return state;
+  }
+};
+
+const removeItem = (index) => {
+  return {
+    type: 'REMOVE_ITEM',
+    index
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
