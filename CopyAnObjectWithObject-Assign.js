78eea92fd4f819cf01aecc161f77a84efb2d21be
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-02-11 17:01:49 -0600

    Using Object.assign to update state

---
+++ CopyAnObjectWithObject-Assign.js
@@ -0,0 +1,27 @@
+const defaultState = {
+  user: 'CamperBot',
+  status: 'offline',
+  friends: '732,982',
+  community: 'freeCodeCamp'
+};
+
+const immutableReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'ONLINE':
+      // don't mutate state here or the tests will fail
+      let obj2 = {
+        status: 'online',
+      }
+      return Object.assign({}, state, obj2);
+    default:
+      return state;
+  }
+};
+
+const wakeUp = () => {
+  return {
+    type: 'ONLINE'
+  }
+};
+
+const store = Redux.createStore(immutableReducer);
